a36f7a52a41a1fce18c6b2db99429477f221429f
YoussefRs <rs.youssef24@gmail.com>
2023-01-01 18:02:32 +0100

    Nx redux

---
+++ netflix-build/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVzot2moeEAYGPV7VnQGU26eZJN3ruSek",
+    authDomain: "netflix-clone-build-1f6bc.firebaseapp.com",
+    projectId: "netflix-clone-build-1f6bc",
+    storageBucket: "netflix-clone-build-1f6bc.appspot.com",
+    messagingSenderId: "83216887830",
+    appId: "1:83216887830:web:f046096b3a1e643a370537"
+  };
